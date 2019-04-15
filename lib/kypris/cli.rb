ab0f66569d76ef5bbd9b36e64681aab4ef7e0f97
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-15 06:54:10 +0000

    add scraper info

--- lib/kypris/cli.rb
+++ lib/kypris/cli.rb
@@ -15,53 +15,24 @@ class CommandLineInterface
   
   def list_products
     puts "Here are our products:"
-    @products = Product.all
+    @products = Product.today
+    @products.each_with_index(1) do |product, i|
+      puts "#{i}. #{product.name}"
+    end
   end
   
   def menu
     input = nil
     while input != "exit" 
       puts "Enter the product you would like more info on, type list to see the products again or exit to leave?"
-      
       input = gets.strip.downcase
-      case input 
-        when "1"
-          puts "more info product 1"
-        when "2"
-          puts "more info on product 2"
-        when "3"
-          puts "more info on product 3"
-        when "4"
-          puts "more info on product 4"
-        when "5"
-          puts "more info on product 5"
-        when "6"
-          puts "more info on product 6"
-        when "7"
-          puts "more info on product 7"
-        when "8"
-          puts "more info on product 8"
-        when "9"
-          puts "more info on product 9"
-        when "10"
-          puts "more info on product 10"
-        when "11"
-          puts "more info on product 11"
-        when "12"
-          puts "more info on product 12"
-        when "13"
-          puts "more info on product 13"
-        when "14"
-          puts "more info on product 14"
-        when "15"
-          puts "more info on product 15"
-        when "16"
-          puts "more info on product 16"
-        when "17"
-          puts "more info on product 17"
-        when "list"
+      
+      if input.to_i > 0 
+        the_product = @product[input.to_i-1]
+        puts "#{the_product.name} - #{the_product.price} - #{the_product.description}"
+      elsif "list"
           list_products
-        else
+      else
           puts "I don't understand, please type list or exit."
       end
     end
--- lib/kypris/product.rb
+++ lib/kypris/product.rb
@@ -1,4 +1,40 @@
 Class Product
   # return a bunch of instances of products
-
+  attr_accessor :name, :price, :url, :description
+  def self.today
+    self.scrape_products
+  end
+    
+  def self.scrape_products
+    # go to Kypris, find products
+    # scrape the properties
+    # instantiate products
+    
+    products = []
+    
+    site = "https://shop.kyprisbeauty.com/"
+    page = Nokogiri::HTML(open(site))
+    product.name = page.css("h4.title").text
+    product.price = page.css("span.price").text.gsub("\n","")
+    product.url = page.css("a.clearfix").first.attr("href")
+    
+    
+  def self.scrape_descriptions
+  
+  end
+    
+    product_1 = Product.new
+    product_1.name =
+    product_1.price =
+    product_1.url =
+    product_1.description =
+    
+    product_2 = Product.new
+    product_2.name =
+    product_2.price =
+    product_2.url =
+    product_2.description =
+    
+    products
+  end
 end
